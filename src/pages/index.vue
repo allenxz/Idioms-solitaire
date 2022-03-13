d137a458dd29548a300d0f54ccf0dbd404317c8c
allenxz <13226271061@163.com>
2022-03-14 00:38:08 +0800

    chore: complete basic logic

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,49 +1,105 @@
 <script setup lang="ts">
-const name = $ref('')
 
-const router = useRouter()
-const go = () => {
-  if (name)
-    router.push(`/hi/${encodeURIComponent(name)}`)
+interface Idiom {
+  derivation?: string
+  example?: string
+  explanation?: string
+  pinyin: string
+  word: string
+  abbreviation?: string
+}
+
+interface Word {
+  explanation?: string
+  more?: string
+  oldword?: string
+  pinyin: string
+  radicals?: string
+  strokes?: string
+  word: string
+}
+
+let isStarted = $ref(false)
+
+let idioms: Idiom[] = reactive([])
+let words: Map<string, string> = reactive([])
+
+onMounted(async() => {
+  let { data: idiomsData } = await useFetch('/data/idiom.json')
+  idiomsData = JSON.parse(idiomsData.value as string).map(w => [w.word, w.pinyin])
+  idioms = new Map(idiomsData)
+
+  let { data: wordData } = await useFetch('/data/word.json')
+  wordData = JSON.parse(wordData.value as string).map(w => [w.word, w.pinyin])
+  words = new Map(wordData)
+})
+
+const getRandomItem = (map: Map) => {
+  const arr = [...map.keys()]
+  const len = arr.length
+  const key = arr[Math.floor(Math.random() * len)]
+  return {
+    word: key,
+    pinyin: map.get(key),
+  }
+}
+
+const displayList: Idiom[] = $ref([])
+
+const startGame = async() => {
+  isStarted = !isStarted
+  displayList.push(getRandomItem(idioms))
+}
+
+let newIdiom = $ref('')
+
+const getXItem = (str: string, separator: string, index: number) => {
+  return str.split(separator).at(index)
+}
+
+const idiom2Pinyin = (idiom) => {
+  return idiom.split('').map(word => words.get(word)).join(' ')
+}
+
+const check = () => {
+  const lastIdiom = displayList.at(-1) as Idiom
+  const lastWord = getXItem(lastIdiom.word, '', 0)
+  const firstWord = getXItem(newIdiom, '', 0)
+  const lastPinyin = getXItem(lastIdiom.pinyin, ' ', -1)
+  const newPinyin = idiom2Pinyin(newIdiom)
+  const firstPinyin = getXItem(newPinyin, ' ', 0)
+  if ((idioms.has(newIdiom) && firstPinyin === lastPinyin) || (firstWord === lastWord)) {
+    displayList.push({
+      word: newIdiom,
+      pinyin: newPinyin,
+    })
+  }
+  newIdiom = ''
 }
 </script>
 
 <template>
   <div>
-    <div i-carbon-campsite text-4xl inline-block />
-    <p>
-      <a rel="noreferrer" href="https://github.com/antfu/vitesse-lite" target="_blank">
-        Vitesse Lite
-      </a>
-    </p>
-    <p>
-      <em text-sm op75>Opinionated Vite Starter Template</em>
-    </p>
-
-    <div py-4 />
-
-    <input
-      id="input"
-      v-model="name"
-      placeholder="What's your name?"
-      type="text"
-      autocomplete="false"
-      p="x-4 y-2"
-      w="250px"
-      text="center"
-      bg="transparent"
-      border="~ rounded gray-200 dark:gray-700"
-      outline="none active:none"
-      @keydown.enter="go"
+    <button
+      v-if="!isStarted"
+      btn
+      @click="startGame"
     >
-
-    <div>
-      <button
-        class="m-3 text-sm btn"
-        :disabled="!name"
-        @click="go"
-      >
-        Go
+      开始游戏
+    </button>
+    <!-- 游戏区域 -->
+    <div v-else flex flex-col items-center gap-3>
+      <ul>
+        <li
+          v-for="word, index of displayList"
+          :key="index"
+        >
+          {{ word.word }}
+        </li>
+      </ul>
+      <input v-model="newIdiom" w-50 type="text" placeholder="输入成语...">
+      <button border-1 w-30 @click="check">
+        确定
       </button>
     </div>
   </div>
